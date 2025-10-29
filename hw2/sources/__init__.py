cc650bea5a1fa54c48ed2532d76a777e2f8c28c3
orkngkc <orkungokce2@gmail.com>
2025-10-29 15:58:04 +0300

    created the deep neural network at CNN.py, added tensorflow to requirements.txt, added py file and funcitons to init.py

--- hw2/sources/__init__.py
+++ hw2/sources/__init__.py
@@ -1,2 +1,4 @@
 from .KNN import KNN
 from .custom_metrics import Metrics
+from .CNN import build_har_model, train_model, evaluate_classification_metrics,\
+      evaluate_on_single_user, predict_model, compile_model
